29873b9ee50af170d5d6aff095ab23b640dc807c
Jonathan <jparrales15@gmail.com>
2017-06-22 04:39:31 +0200

    Update Lista_Ultimo.c
    cambio ultumo

--- src/Lista_Ultimo.c
+++ src/Lista_Ultimo.c
@@ -4,7 +4,7 @@
 
 extern ElementoLista *Lista_Ultimo(ListaEnlazada *lista)
 {
-    ElementoLista *tmp;
+       ElementoLista *tmp;
     tmp=(ElementoLista *)malloc(sizeof(ElementoLista *));
      if(lista->numeroElementos==0){
         return NULL;
@@ -13,16 +13,7 @@ extern ElementoLista *Lista_Ultimo(ListaEnlazada *lista)
         return lista->ancla;
     }
     else{
-        tmp->siguiente=lista->ancla->siguiente;
-        tmp->anterior=lista->ancla->anterior;
-        for(int i=0;i<lista->numeroElementos;i++)
-        {
-            tmp->anterior=tmp->siguiente;
-            tmp->siguiente=tmp->siguiente->siguiente;
-            if(tmp->siguiente==NULL)
-            {
-                return tmp->anterior;
-            }
-        }
+        return lista->ancla->anterior;
+        
     }
 }
