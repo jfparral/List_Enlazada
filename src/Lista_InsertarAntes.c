1f22e32719450b13742294c9e08e08de70cb2fbf
luisxmetalx <you@example.com>
2017-07-30 10:04:42 -0500

    lista enlazada terminada

--- src/Lista_InsertarAntes.c
+++ src/Lista_InsertarAntes.c
@@ -1,35 +1,32 @@
 #include "miLista.h"
+#include <stdlib.h>
 
-extern int Lista_InsertarAntes(ListaEnlazada *lista, void *objeto, ElementoLista *elemento)
-{
-    ElementoLista *nuevo,*tmp;
-    nuevo=(ElementoLista *)malloc(sizeof(ElementoLista));
-    tmp=(ElementoLista *)malloc(sizeof(ElementoLista));
-    if(Lista_Buscar(*lista,elemento->objeto)!=NULL && Lista_Buscar(*lista,*objeto)!=NULL)
-    {
-        if(elemento->anterior==NULL)
-        {
-            nuevo->objeto=*objeto;
-            tmp->anterior=elemento->anterior;
-            nuevo->anterior=elemento->anterior;
-            nuevo->siguiente=*elemento;
-            elemento->anterior=*nuevo;
-            lista->numeroElementos++;
-            return 1;
-        }
-        else{
-            nuevo->objeto=*objeto;
-            tmp->anterior=elemento->anterior;
-            tmp->anterior->siguiente=*nuevo;
-            nuevo->anterior=elemento->anterior;
-            nuevo->siguiente=*elemento;
-            elemento->anterior=*nuevo;
-            lista->numeroElementos++;
-            return 1;
-        }
-    }
-    else
-    {
-        return 0;
-    }
+extern int Lista_InsertarAntes(ListaEnlazada *lista, void *objeto, ElementoLista *elemento){
+	int i;
+	ElementoLista *actual = (ElementoLista*)malloc(sizeof(ElementoLista));
+	ElementoLista *nuevo_elemento = (ElementoLista*)malloc(sizeof(ElementoLista));
+	actual = lista->ancla.siguiente;
+	nuevo_elemento->objeto = objeto;
+	for (i = 0; i <= lista->numeroElementos; i++){
+		if (actual == elemento){
+			ElementoLista *ant = (ElementoLista*)malloc(sizeof(ElementoLista));
+			ant = actual->anterior;
+			ant->siguiente = nuevo_elemento;
+			nuevo_elemento->anterior = ant;
+			nuevo_elemento->siguiente = actual;
+			actual->anterior = nuevo_elemento;
+			lista->numeroElementos++;
+			//printf("%d\n", i);
+			return 1;
+		}else{
+			if (actual->anterior == &lista->ancla){
+				Lista_InsertarInicio(lista,objeto);
+				return 1;
+			}
+		}
+		
+		actual = actual->siguiente;
+	}
+	return 0;
 }
+
